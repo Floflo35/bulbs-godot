af31dc3fef25c317ae1b86f2e62293e1bf0be31c
Florent Chardevel <fchardevel@gmail.com>
2024-10-13 22:27:34 +0200

    Textures and bulb anim

--- main.gd
+++ main.gd
@@ -7,28 +7,9 @@ extends Node
 func _ready() -> void:
 	$UserInterface/Retry.hide()
 
-func _on_mob_timer_timeout() -> void:
-	# Create a new instance of the mob scene
-	var mob = mob_scene.instantiate()
-	
-	# Choose a random location on the SpawnPath
-	# We store the reference to the SpawnLocation node.
-	var mob_spawn_location = get_node("SpawnPath/SpawnLocation")
-	# And give it a random offset.
-	mob_spawn_location.progress_ratio = randf()
-	
-	var player_position = $Player.position
-	mob.initialize(mob_spawn_location.position, player_position)
-	
-	# Spawn the mob by adding it to the Main scene.
-	add_child(mob)
-	
-	# We connect the mob to the score label to update the score upon squashing one.
-	mob.squashed.connect($UserInterface/ScoreLabel._on_mob_squashed.bind())
-
-
 func _on_player_hit() -> void:
-	$MobTimer.stop()
+	#$MobTimer.stop()
+	# Show the "retry" menu
 	$UserInterface/Retry.show()
 	
 func _unhandled_input(event: InputEvent) -> void:
