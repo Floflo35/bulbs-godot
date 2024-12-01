4e5300f740c6306d5a0ec65b46a5917d5fd09005
Florent Chardevel <fchardevel@gmail.com>
2024-12-01 18:57:51 +0100

    Fixed pickup item I added the "BulbCollect" scene to the "collectibles" scene group, and now I can pick it up! I'm not sure I understand why though.

--- main.gd
+++ main.gd
@@ -1,6 +1,6 @@
 extends Node
 
-# Tutorial done!
+# Tutorial done! test
 
 @export var mob_scene: PackedScene
 
@@ -12,12 +12,12 @@ func _on_player_hit() -> void:
 	#$MobTimer.stop()
 	# Show the "retry" menu
 	$UserInterface/Retry.show()
-	
+
 func _unhandled_input(event: InputEvent) -> void:
 	if event.is_action_pressed("ui_accept") and $UserInterface/Retry.visible:
 		# This restarts the current scene.
 		get_tree().reload_current_scene()
-		
+
 
 func _on_bulb_collect_picked() -> void:
 	print("collectible picked up! (main.gd)")
