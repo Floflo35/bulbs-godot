0f395b47139a9d61fd2bf5a9ed214c125c00b21c
Florent Chardevel <fchardevel@gmail.com>
2024-12-09 13:13:57 +0100

    Cleaner camera movement code

--- camera_pivot.gd
+++ camera_pivot.gd
@@ -4,6 +4,7 @@ extends StaticBody3D
 var target_coordinates
 var direction
 var distance_to_target
+var camera_moving = true
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -12,21 +13,20 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	# if direction != Vector3.ZERO:
-	# Move the camera in the direction of the new camera pivot
-	direction = (target_coordinates - position).normalized()
-	distance_to_target = (target_coordinates - position).length()
-	print("distance = ", distance_to_target)
-	if distance_to_target > 1:
-		position += (direction * speed * delta)
-	else:
-		position = target_coordinates
+	if camera_moving == true:
+		# Move the camera in the direction of the new camera pivot
+		direction = (target_coordinates - position).normalized()
+		distance_to_target = (target_coordinates - position).length()
+		print("distance = ", distance_to_target)
+		if distance_to_target > 1:
+			position += (direction * speed * delta)
+		else:
+			position = target_coordinates
+			camera_moving = false
 	
 
 
 func _on_player_camera_screen(target_position: Variant) -> void:
 	if target_position != Vector3.ZERO:
 		target_coordinates = target_position
-		# print("bop ", target_position.x, " ", target_position.z)
-		# print("bip ", self.position.x, " ", self.position.z)
-		# direction = (target_position - position).normalized()
+		camera_moving = true
